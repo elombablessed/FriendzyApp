a5be1ea1da397eef03dfbf5e171717addbec79c1
Elomba Blessed <blessedelomba@gmail.com>
2020-08-18 19:33:17 +0100

    This is my first commit file

---
+++ friendzyapp/src/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import Searchbox from './Searchbox'
+import './App.css';
+import { robots } from './robots';
+
+
+
+class  App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({robots: users}));
+        
+    }
+
+        onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+
+
+        return (<div className='tc'>
+            <h1 className='f1'>Friendzy</h1>
+            <Searchbox searchChange = {this.onSearchChange}/>
+            <CardList robots={filteredRobots}/>
+        </div>);
+
+    }
+   
+}
+
+export default App;
